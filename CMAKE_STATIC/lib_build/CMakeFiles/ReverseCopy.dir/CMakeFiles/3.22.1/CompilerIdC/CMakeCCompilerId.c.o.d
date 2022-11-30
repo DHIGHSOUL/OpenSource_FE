70ce1899d5894ec4f99f1bbc8205afa33e6517fd
DHIGHSOUL <highsoul1227@gmail.com>
2022-11-30 13:47:09 +0900

    Feat: CMAKE_STATIC

---
+++ CMAKE_STATIC/lib_build/CMakeFiles/ReverseCopy.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ReverseCopy.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o: \
+ /home/rolf/rolfdir/CMAKE_STATIC/lib_build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c \
+ /usr/include/stdc-predef.h
