9ce555c57d09718bcf499003e27a9c1070656379
DHGIGHSOUL <highsoul1227@gmail.com>
2022-12-06 15:09:40 +0900

    Final test

---
+++ Test/P1.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 메뉴에 따라 해당 명령어를 실행한다.
+
+echo 명령어 메뉴
+cat << MENU
+    p : 현재 디렉터리의 절대 경로 보여주기
+    w : 현재 userid 보여주기
+    u : 시스템정보 보여주기
+    q : 끝냄
+MENU
+stop=0
+
+while (( $stop == 0 ))
+do
+    echo -n '? '
+    read reply
+    case $reply in
+        "p") pwd;;
+        "w") who;;
+        "u") uname -a;;
+        "q") stop=1;;
+        *) echo 잘못된 선택::
+    esac
+done
